ed9d7f646bbcc55904ec5e5ce3fdd347afe15a5c
gerardo <glomasvelazco@csustan.edu>
2020-04-22 22:11:08 -0700

    Java Driver connection

---
+++ mongoJava/src/com/POS/mongo/JavaMongoConnection.java
@@ -0,0 +1,32 @@
+package com.POS.mongo;
+
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+
+
+public class JavaMongoConnection
+{
+
+	public static void main (String args[])
+	{
+	
+		//Connect to Server
+			MongoClient mongoClient = MongoClients.create(
+			    "mongodb+srv://gerardo:group4@cluster0-a37wq.mongodb.net/POSsystem?retryWrites=true&w=majority");
+			MongoDatabase database = mongoClient.getDatabase("POSsystem");
+			System.out.println("server connection successful");
+			
+		//List Collections in Database
+			MongoIterable<String> collections = database.listCollectionNames();
+				for (String name: collections)
+					{
+					    System.out.println(name);
+					}
+
+	}
+
+	
+
+}
